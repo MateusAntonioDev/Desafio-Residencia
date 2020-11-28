00249a58b429610e8e1e93243b3beec7029b7068
AlandaAquino <alandaoaquino@fac.pe.senac.br>
2020-11-28 00:31:20 -0300

    Integramos o banco de dados a aplicação

---
+++ Teste4/Teste4/Controllers/MadrinhaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Teste4.Models;
+
+namespace Teste4.Controllers
+{
+    public class MadrinhaController : Controller
+    {
+        // GET: Madrinha
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetList()
+        {
+            using (DBModels db = new DBModels()) 
+            {
+                var mdrList = db.Madrinha.ToList<Madrinha>();
+                return Json(new { data = mdrList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
